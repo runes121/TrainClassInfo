0e634be693c2682ea50c9ea9c2efde7287bbb6d3
runes121 <75335579+runes121@users.noreply.github.com>
2023-09-17 13:28:28 +0100

    Automated process. Now pulls directly from Wikipedia so that there shouldn't be any missing classes.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter
 import ttkbootstrap as ttk
-import traininfo
+import wikipedia
 
 
 window = ttk.Window(themename="darkly")
@@ -12,14 +12,15 @@ class_input.pack(pady=5)
 
 
 def change_info():
-    summary = traininfo.get_train_summary(class_input.get().lower())
+    print(f"Searching for: British Rail Class {class_input.get()}")
+    summary = wikipedia.summary(f"British Rail Class {class_input.get()}", sentences=2, auto_suggest=False, redirect=False)
     information_box.config(text=summary)
 
 
 get_info_button = ttk.Button(window, text="Get info!", command=change_info)
 get_info_button.pack(pady=5)
 
-information_box = ttk.Label(window, text="Enter a class to get information.", wraplength=470, font=("Arial", 10))
+information_box = ttk.Label(window, text="Enter a class to get information.", wraplength=470, font=("Arial", 13))
 information_box.pack(pady=5)
 
 warning_label = ttk.Label(window, text="Warning: Summaries may contain minimal error.", font=("Arial", 10), foreground="dark red")

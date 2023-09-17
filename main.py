f29ba0b40d6a25eb423ce99d150747f688768708
runes121 <75335579+runes121@users.noreply.github.com>
2023-09-17 13:50:27 +0100

    Added error handling.

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ class_input.pack(pady=5)
 
 def change_info():
     print(f"Searching for: British Rail Class {class_input.get()}")
-    summary = wikipedia.summary(f"British Rail Class {class_input.get()}", sentences=2, auto_suggest=False, redirect=False)
-    information_box.config(text=summary)
+    try:
+        summary = wikipedia.summary(f"British Rail Class {class_input.get()}", sentences=2, auto_suggest=False, redirect=False)
+        information_box.config(text=summary)
+    except wikipedia.exceptions.PageError:
+        information_box.config(text="Couldn't find class info. Does it even exist?")
+    except wikipedia.exceptions.RedirectError:
+        information_box.config(text="A redirect occurred, this most likely means this train class doesn't exist.")
+    except Exception as e:
+        information_box.config(text=f"An unknown error occurred. {e}")
 
 
 get_info_button = ttk.Button(window, text="Get info!", command=change_info)
@@ -23,8 +30,8 @@ get_info_button.pack(pady=5)
 information_box = ttk.Label(window, text="Enter a class to get information.", wraplength=470, font=("Arial", 13))
 information_box.pack(pady=5)
 
-warning_label = ttk.Label(window, text="Warning: Summaries may contain minimal error.", font=("Arial", 10), foreground="dark red")
-warning_label.pack()
-warning_label.place(relx=0.5, rely=0.95, anchor="center")
+wikipedia_credit = ttk.Label(window, text="Summaries taken from Wikipedia.", font=("Arial", 10))
+wikipedia_credit.pack()
+wikipedia_credit.place(relx=0.5, rely=0.95, anchor="center")
 
 tkinter.mainloop()
